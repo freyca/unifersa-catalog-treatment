4271866901bfca6b334c8c7200e192305ada51e6
Fran Rey <franreycastedo@gmail.com>
2025-09-11 13:38:58 +0200

    add: only improve texts with openai when asked for

--- industrial-cron.sh
+++ industrial-cron.sh
@@ -7,10 +7,10 @@ function main()
 {
     $PHP_BIN unifersa u:download-csv &>/dev/null || exit 1
     $PHP_BIN unifersa u:export-discontinued-products-to-csv &>/dev/null || exit 1
-    $PHP_BIN unifersa u:improve-texts-with-ai &>/dev/null || exit 1
 
     if [[ "$1" -eq 'full' ]]
     then
+        $PHP_BIN unifersa u:improve-texts-with-ai &>/dev/null || exit 1
         $PHP_BIN unifersa u:export-db-to-csv --last &>/dev/null || exit 1
     fi
 
