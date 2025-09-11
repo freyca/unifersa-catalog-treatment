c1c43b4dfbdeedd169ae83f48b884ac62146ada1
Fran Rey <franreycastedo@gmail.com>
2025-09-11 13:10:46 +0200

    add: bash script

---
+++ industrial-cron.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+W_DIR='/home/casqui/industrial-ferretera-ia'
+PHP_BIN='/usr/bin/php83'
+
+function main()
+{
+    $PHP_BIN unifersa u:download-csv &>/dev/null || exit 1
+    $PHP_BIN unifersa u:export-discontinued-products-to-csv &>/dev/null || exit 1
+    $PHP_BIN unifersa u:improve-texts-with-ai &>/dev/null || exit 1
+
+    if [[ "$1" -eq 'full' ]]
+    then
+        $PHP_BIN unifersa u:export-db-to-csv &>/dev/null || exit 1
+    fi
+
+    exit 0
+}
+
+main "$@"
